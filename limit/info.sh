9f261df56389c34afb9a70deee980e19ff2d4b47
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-11-16 10:19:20 +0700

    Add files via upload

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "SF Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script By AdminSF" | lolcat
+echo "                   Author Script AdminSF" | lolcat
+echo "-----------------------------------------------------"
